e7e31171ccdd1e88d66e64bc8a8f9bed03a91c25
Siddhant Pandey <37979764+tnahddisttud@users.noreply.github.com>
2024-05-09 12:02:45 +0530

    Update main.py

--- anomaly_detector/main.py
+++ anomaly_detector/main.py
@@ -1,8 +1,10 @@
 from detector import AnomalyDetector
 
-
-detector = AnomalyDetector()
-detector.preprocess_data()
-detector.train_models()
-anomaly_scores = detector.calculate_anomaly_scores()
-threshold = detector.evaluate_threshold(anomaly_scores)
+if __name__ == "__main__":
+  detector = AnomalyDetector()
+  detector.preprocess_data()
+  detector.train_models()
+  anomaly_scores = detector.calculate_anomaly_scores()
+  threshold = detector.evaluate_threshold(anomaly_scores)
+  # Once model is trained, you can get the threshold value and make prediction
+  # detector.predict(row, threshold)
